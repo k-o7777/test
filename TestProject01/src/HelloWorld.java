2dd421b58e96352dfbd0d5fb5a9a13d24f0d5d2a
k-o7777 <kaneyuki.1994.922@gmail.com>
2019-08-22 10:31:52 +0900

    2019.08.19

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// HelloWorld! の出力
+		System.out.println("HelloWorld!");
+
+		String [] name = {"田中","高橋","斉藤"};
+		name[0]="加藤";
+
+		System.out.println(name[0]);
+
+	}
+
+}
